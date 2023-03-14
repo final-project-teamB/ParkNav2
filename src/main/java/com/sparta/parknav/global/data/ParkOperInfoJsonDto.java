6bc652525c239ac67d181ef270eb6f3bd615c213
doyeonKim <0915dy@gmail.com>
2023-03-14 18:29:37 +0900

    Feat: #10 json 파일을 담을 DTO 구현
    주차장 정보, 상세정보를 담은 Json 파일의 key 값을 DTO에 필드로 선언했다.

---
+++ src/main/java/com/sparta/parknav/global/data/ParkOperInfoJsonDto.java
@@ -0,0 +1,19 @@
+package com.sparta.parknav.global.data;
+
+import lombok.Getter;
+
+@Getter
+public class ParkOperInfoJsonDto {
+
+    private String weekdayOpen;
+    private String weekdayClose;
+    private String satOpen;
+    private String satClose;
+    private String sunOpen;
+    private String sunClose;
+    private String chargeBsTime;
+    private String chargeBsChrg;
+    private String chargeAditUnitTime;
+    private String chargeAditUnitChrg;
+    private String cmprtCo;
+}
