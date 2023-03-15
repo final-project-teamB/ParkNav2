1e6c1a63909ca9994f99545f4fedfbd92f045cd7
doyeonKim <0915dy@gmail.com>
2023-03-15 16:20:34 +0900

    Feat: #18 로그인 기능 구현
    - 일반 사용자 로그인, 관리자 로그인으로 나누어 구현
    - 아이디, 비밀번호가 일치하지 않을 경우 ErrorType 추가

--- src/main/java/com/sparta/parknav/global/exception/ErrorType.java
+++ src/main/java/com/sparta/parknav/global/exception/ErrorType.java
@@ -9,7 +9,8 @@ public enum ErrorType {
     NOT_TOKEN(401, "토큰이 없습니다."),
     INVALID_ARGUMENT(400, "비어있는 항목이 있습니다."),
     NOT_FOUND_USER(401, "등록된 사용자가 없습니다."),
-    DUPLICATED_USERID(400, "중복된 아이디입니다.")
+    DUPLICATED_USERID(400, "중복된 아이디입니다."),
+    NOT_MATCHING_INFO(401, "아이디 또는 비밀번호를 잘못 입력했습니다.")
     ;
     private int code;
     private String msg;
