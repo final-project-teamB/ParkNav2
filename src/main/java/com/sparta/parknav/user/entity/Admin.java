c3c34503966fe32d45a5d56d6496a85073889ae3
doyeonKim <0915dy@gmail.com>
2023-03-15 11:14:14 +0900

    Feat: #16 Admin 엔티티 분리
    일반사용자와 관리자의 사용 페이지가 완전히 다르므로 테이블을 분리해서 구성한다.

---
+++ src/main/java/com/sparta/parknav/user/entity/Admin.java
@@ -0,0 +1,29 @@
+package com.sparta.parknav.user.entity;
+
+import com.sparta.parknav.parking.entity.ParkInfo;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Table(name = "ADMINS")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 30)
+    private String adminId;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "park_info_id")
+    private ParkInfo parkInfo;
+}
