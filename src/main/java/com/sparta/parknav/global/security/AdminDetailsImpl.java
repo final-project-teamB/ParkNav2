166f249f0e96f4c0037f980e07a5065d1edf8f52
doyeonKim <0915dy@gmail.com>
2023-03-15 16:17:34 +0900

    Feat: #18 관리자 로그인 security 인증 구현
    - 관리자 인증을 위해 `JwtAuthAdminFilter`, `JwtUtilAdmin`, `AdminDetailsImpl`, `AdminDetailsServiceImpl` 추가
    - `JwtAuthFilter` 전에 `JwtAuthAdminFilter`를 거친다.
    - 관리자 권한이 필요한 요청인 '/api/mgt'인 경우에만 `JwtAuthAdminFilter`를 수행한다.
    - 관리자 로그인 요청시 permitAll 되도록 추가했다.

---
+++ src/main/java/com/sparta/parknav/global/security/AdminDetailsImpl.java
@@ -0,0 +1,66 @@
+package com.sparta.parknav.global.security;
+
+import com.sparta.parknav.user.entity.Admin;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class AdminDetailsImpl implements UserDetails {
+
+    private final Admin admin;
+    private final String adminId;
+
+    // 인증이 완료된 사용자 추가하기
+    public AdminDetailsImpl(Admin admin, String adminId) {
+        this.admin = admin;
+        this.adminId = adminId;
+    }
+
+    public Admin getUser() {
+        return admin;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority("ROLE_ADMIN");
+        Collection<GrantedAuthority> authorities = new ArrayList<>();   // 사용자 권한을 GrantedAuthority 로 추상화
+        authorities.add(simpleGrantedAuthority);
+
+        return authorities; // GrantedAuthority 로 추상화된 사용자 권한 반환
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.adminId;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+}
