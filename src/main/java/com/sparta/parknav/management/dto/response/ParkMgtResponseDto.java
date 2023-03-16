d496bc07865acc358247183627f7a463497bf737
Seyoung Oh <osy9536@naver.com>
2023-03-16 15:52:11 +0900

    Feat : #36 주차 현황 조회 API 구현
    close #36

--- src/main/java/com/sparta/parknav/management/dto/response/ParkMgtResponseDto.java
+++ src/main/java/com/sparta/parknav/management/dto/response/ParkMgtResponseDto.java
@@ -1,7 +1,9 @@
 package com.sparta.parknav.management.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -9,7 +11,9 @@ import java.time.LocalDateTime;
 public class ParkMgtResponseDto {
 
     private String carNum;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime enterTime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime exitTime;
     private int charge;
 
@@ -21,5 +25,14 @@ public class ParkMgtResponseDto {
         this.enterTime = enterTime;
         this.exitTime = exitTime;
         this.charge = charge;
+    }
+
+    public static ParkMgtResponseDto of(String carNum, LocalDateTime enterTime, LocalDateTime exitTime, int charge) {
+        return builder()
+                .carNum(carNum)
+                .enterTime(enterTime)
+                .exitTime(exitTime)
+                .charge(charge)
+                .build();
     }
 }
