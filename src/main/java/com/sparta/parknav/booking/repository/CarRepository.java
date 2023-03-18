1e0815b08e21a7b814ebf610afc5c4403710915f
JaeHun <clickyour@gmail.com>
2023-03-18 23:11:43 +0900

    Feat : #69 유저 차량 조회 기능 추가

--- src/main/java/com/sparta/parknav/booking/repository/CarRepository.java
+++ src/main/java/com/sparta/parknav/booking/repository/CarRepository.java
@@ -4,6 +4,7 @@ import com.sparta.parknav.booking.entity.Car;
 import com.sparta.parknav.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CarRepository extends JpaRepository<Car, Long> {
@@ -15,4 +16,6 @@ public interface CarRepository extends JpaRepository<Car, Long> {
     boolean existsByUser(User user);
 
     Car findByUserAndCarNum(User user, String carNum);
+
+    List<Car> findByUserIdOrderByIsUsingDesc(Long userId);
 }
