eb7887657d8f5a9c997910c3f1cfcf68ec3beadd
JaeHun <clickyour@gmail.com>
2023-04-05 13:50:04 +0900

    feat : #155 각 구획이 가득차있을경우 예외 적용
    - 예약 -> 공통 가득찼을경우 옮기지않음
    - 일반 -> 예약 가득찼을경우 옮기지않음

--- src/main/java/com/sparta/parknav/management/service/SchedueldMgtService.java
+++ src/main/java/com/sparta/parknav/management/service/SchedueldMgtService.java
@@ -4,15 +4,19 @@ import com.sparta.parknav._global.exception.CustomException;
 import com.sparta.parknav._global.exception.ErrorType;
 import com.sparta.parknav.booking.entity.ParkBookingInfo;
 import com.sparta.parknav.booking.repository.ParkBookingInfoRepository;
+import com.sparta.parknav.management.dto.ParkSpaceInfo;
 import com.sparta.parknav.management.entity.ParkMgtInfo;
 import com.sparta.parknav.management.entity.ZoneType;
 import com.sparta.parknav.management.repository.ParkMgtInfoRepository;
+import com.sparta.parknav.parking.entity.ParkOperInfo;
+import com.sparta.parknav.parking.repository.ParkOperInfoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.swing.text.html.Option;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
@@ -25,6 +29,8 @@ public class SchedueldMgtService {
 
     private final ParkBookingInfoRepository parkBookingInfoRepository;
     private final ParkMgtInfoRepository parkMgtInfoRepository;
+    private final ParkOperInfoRepository parkOperInfoRepository;
+    private final MgtService mgtService;
 
     @Scheduled(cron = "0 0 * * * *")
     @Transactional
@@ -38,11 +44,21 @@ public class SchedueldMgtService {
         List<ParkBookingInfo> bookingInfoStartTimeCarList = parkBookingInfoRepository.findAllByStartTimeEquals(DateTimeHour);
         for (ParkBookingInfo bookingInfoStartTimeCar : bookingInfoStartTimeCarList) {
             // 가져온 예약정보 중 이미 입차한 차량 중 일반구획에 있는 차량 리스트를 가져옴
-            Optional<ParkMgtInfo> parkMgtInfoCheck = parkMgtInfoRepository.findByParkInfoIdAndCarNumAndExitTimeNullAndZone(bookingInfoStartTimeCar.getParkInfo().getId(),bookingInfoStartTimeCar.getCarNum(),ZoneType.GENERAL);
-            if (parkMgtInfoCheck.isPresent()){
+            Optional<ParkMgtInfo> parkMgtInfoCheck = parkMgtInfoRepository.findByParkInfoIdAndCarNumAndExitTimeNullAndZone(bookingInfoStartTimeCar.getParkInfo().getId(), bookingInfoStartTimeCar.getCarNum(), ZoneType.GENERAL);
+            if (parkMgtInfoCheck.isPresent()) {
                 ParkMgtInfo parkMgtInfo = parkMgtInfoCheck.get();
-                // 차량 구획을 예약구획으로 이동
-                parkMgtInfo.zoneUpdate(ZoneType.BOOKING);
+                Optional<ParkOperInfo> parkOperInfoCheck = parkOperInfoRepository.findByParkInfoId(bookingInfoStartTimeCar.getParkInfo().getId());
+                // 주차장 구획 별 여유공간을 가져옴
+                ParkSpaceInfo useSpaceInfo = mgtService.getUseSpaceInfo(bookingInfoStartTimeCar.getParkInfo());
+                ParkSpaceInfo spaceInfo = ParkSpaceInfo.of(0, 0, 0);
+                if (parkOperInfoCheck.isPresent()) {
+                    spaceInfo = mgtService.getParkSpaceInfo(parkOperInfoCheck.get());
+                }
+                // 여유 공간이 있을때만 이동
+                if (useSpaceInfo.getBookingCarSpace() < spaceInfo.getBookingCarSpace()) {
+                    // 차량 구획을 예약구획으로 이동
+                    parkMgtInfo.zoneUpdate(ZoneType.BOOKING);
+                }
             }
         }
 
@@ -50,11 +66,21 @@ public class SchedueldMgtService {
         List<ParkBookingInfo> bookingInfoEndTimeCarList = parkBookingInfoRepository.findAllByEndTimeBetween(startTime, DateTimeHour);
         for (ParkBookingInfo bookingInfoEndTimeCar : bookingInfoEndTimeCarList) {
             // 예약시간이 초과 된 예약정보 중 아직 출차하지 않고 예약구획에 있는 차량 관리정보를 가져옴
-            Optional<ParkMgtInfo> parkMgtInfoCheck = parkMgtInfoRepository.findByParkBookingInfoIdAndExitTimeNullAndZone(bookingInfoEndTimeCar.getId(),ZoneType.BOOKING);
+            Optional<ParkMgtInfo> parkMgtInfoCheck = parkMgtInfoRepository.findByParkBookingInfoIdAndExitTimeNullAndZone(bookingInfoEndTimeCar.getId(), ZoneType.BOOKING);
             if (parkMgtInfoCheck.isPresent()) {
                 ParkMgtInfo parkMgtInfo = parkMgtInfoCheck.get();
-                // 차량을 공통 구획으로 이동
-                parkMgtInfo.zoneUpdate(ZoneType.COMMON);
+                Optional<ParkOperInfo> parkOperInfoCheck = parkOperInfoRepository.findByParkInfoId(bookingInfoEndTimeCar.getParkInfo().getId());
+                // 주차장 구획 별 여유공간을 가져옴
+                ParkSpaceInfo useSpaceInfo = mgtService.getUseSpaceInfo(bookingInfoEndTimeCar.getParkInfo());
+                ParkSpaceInfo spaceInfo = ParkSpaceInfo.of(0, 0, 0);
+                if (parkOperInfoCheck.isPresent()) {
+                    spaceInfo = mgtService.getParkSpaceInfo(parkOperInfoCheck.get());
+                }
+                // 여유 공간이 있을때만 이동
+                if (useSpaceInfo.getCommonCarSpace() < spaceInfo.getCommonCarSpace()) {
+                    // 차량을 공통 구획으로 이동
+                    parkMgtInfo.zoneUpdate(ZoneType.COMMON);
+                }
             }
         }
     }
