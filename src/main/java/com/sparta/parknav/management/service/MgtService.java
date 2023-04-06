752bbd0330be13bf2203352a6155fc4333e02d5e
Seyoung Oh <76714304+osy9536@users.noreply.github.com>
2023-04-06 22:25:59 +0900

    Test : 테스트 코드

--- src/main/java/com/sparta/parknav/management/service/MgtService.java
+++ src/main/java/com/sparta/parknav/management/service/MgtService.java
@@ -78,32 +78,6 @@ public class MgtService {
 //            redisLockRepository.unlock(requestDto.getParkId());
 //        }
 
-    public CarInResponseDto enterLogic(CarNumRequestDto requestDto, Admin user) {
-        if (requestDto.getParkId() == null) {
-            throw new CustomException(ErrorType.CONTENT_IS_NULL);
-        }
-        RLock lock = redissonClient.getLock("EnterLock" + requestDto.getParkId());
-        try {
-            //선행 락 점유 스레드가 존재하면 waitTime동안 락 점유를 기다리며 leaseTime 시간 이후로는 자동으로 락이 해제되기 때문에 다른 스레드도 일정 시간이 지난 후 락을 점유할 수 있습니다.
-            if (!lock.tryLock(30, 10, TimeUnit.SECONDS)) {
-                log.info("락 획득 실패");
-                throw new CustomException(ErrorType.FAILED_TO_ACQUIRE_LOCK);
-            }
-            log.info("락 획득 성공");
-            return enterLogic(requestDto, user);
-        } catch (InterruptedException e) {
-            log.info("락 획득 대기 중 인터럽트 발생");
-            Thread.currentThread().interrupt();
-            throw new CustomException(ErrorType.INTERRUPTED_WHILE_WAITING_FOR_LOCK);
-        } finally {
-            log.info("finally문 실행");
-            if (lock != null && lock.isLocked() && lock.isHeldByCurrentThread()) {
-                lock.unlock();
-                log.info("언락 실행");
-            }
-        }
-    }
-
     @Transactional
     public CarInResponseDto enterLogic(CarNumRequestDto requestDto, Admin user) {
 
