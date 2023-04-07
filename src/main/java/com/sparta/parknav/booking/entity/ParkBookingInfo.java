df45fcbad345cf040d6ba90fd9057bbb7155a6d7
JaeHun <clickyour@gmail.com>
2023-04-07 11:37:22 +0900

    feat : #162 입차 관련 로직 수정
    - 즉시 예약 단계에서 예약시간보다 1시간 빠르게 입장할경우 예약시간 수정로직 추가
    - 기존 예약에 시간을 추가함으로 현재 시간에 대한 available 검증단계 추가
    - available 상태에 따른 처리 구현

--- src/main/java/com/sparta/parknav/booking/entity/ParkBookingInfo.java
+++ src/main/java/com/sparta/parknav/booking/entity/ParkBookingInfo.java
@@ -76,4 +76,8 @@ public class ParkBookingInfo {
                 .build();
     }
 
+    public void startTimeUpdate(int hour){
+        this.startTime = this.startTime.minusHours(hour);
+    }
+
 }
