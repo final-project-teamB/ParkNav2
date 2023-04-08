cb228f397083051ff7fd0b3075937935385edfe2
JaeHun <clickyour@gmail.com>
2023-04-08 10:09:19 +0900

    feat : #172 v1 프론트 입, 출차 기능 구현
    - 프론트 관리자페이지 입 출차 기능구현

--- src/main/java/com/sparta/parknav/management/dto/response/ParkMgtListResponseDto.java
+++ src/main/java/com/sparta/parknav/management/dto/response/ParkMgtListResponseDto.java
@@ -9,16 +9,20 @@ public class ParkMgtListResponseDto {
     private Page page;
     private String parkName;
 
+    private Long parkId;
+
     @Builder
-    private ParkMgtListResponseDto(Page page, String parkName) {
+    private ParkMgtListResponseDto(Page page, String parkName, Long parkId) {
         this.parkName = parkName;
         this.page = page;
+        this.parkId = parkId;
     }
 
-    public static ParkMgtListResponseDto of(Page page, String parkName) {
+    public static ParkMgtListResponseDto of(Page page, String parkName, Long parkId) {
         return builder()
                 .page(page)
                 .parkName(parkName)
+                .parkId(parkId)
                 .build();
     }
 }
