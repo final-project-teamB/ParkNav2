ebff7a5318492d9d92b2609fc7523342823a6ec6
JaeHun <clickyour@gmail.com>
2023-04-10 16:48:19 +0900

    feat : #174 v1 입차, 예약 동시성제어 테스트코드
    - 입차, 예약 동시성제어 테스트코드 작성

---
+++ src/test/java/com/sparta/parknav/booking/service/BookingServiceTest.java
@@ -0,0 +1,114 @@
+package com.sparta.parknav.booking.service;
+
+import com.sparta.parknav._global.exception.CustomException;
+import com.sparta.parknav.booking.dto.BookingInfoRequestDto;
+
+import com.sparta.parknav.booking.entity.Car;
+import com.sparta.parknav.booking.repository.CarRepository;
+
+import com.sparta.parknav.management.dto.ParkSpaceInfo;
+import com.sparta.parknav.management.service.MgtService;
+import com.sparta.parknav.parking.entity.ParkInfo;
+import com.sparta.parknav.parking.entity.ParkOperInfo;
+import com.sparta.parknav.parking.repository.ParkInfoRepository;
+import com.sparta.parknav.parking.repository.ParkOperInfoRepository;
+import com.sparta.parknav.user.entity.User;
+import com.sparta.parknav.user.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@ActiveProfiles("test")
+@Slf4j
+@SpringBootTest
+class BookingServiceTest {
+
+    @Autowired
+    private BookingService bookingService;
+    @Autowired
+    private ParkInfoRepository parkInfoRepository;
+    @Autowired
+    private ParkOperInfoRepository parkOperInfoRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private CarRepository carRepository;
+    @Autowired
+    private MgtService mgtService;
+
+    @Test
+    void bookingPark() throws InterruptedException {
+        //when
+        ParkInfo parkInfo = parkInfoRepository.save(ParkInfo.of("테스트주차장", "테스트주소1", "테스트주소2", "33.2501489768202", "126.563230508718"));
+        ParkOperInfo parkOperInfoTmp = ParkOperInfo.of(parkInfo, "민영");
+        parkOperInfoTmp.update("00:00", "23:59","00:00", "23:59","00:00", "23:59",30, 1000, 30, 500, 20);
+        parkOperInfoRepository.save(parkOperInfoTmp);
+        int numOfUsers = 20;
+        // 대기하는 스레드의 숫자를 지정
+        CountDownLatch endLatch = new CountDownLatch(numOfUsers);
+        ExecutorService executorService = Executors.newFixedThreadPool(numOfUsers);
+        AtomicInteger successCount = new AtomicInteger();
+        AtomicInteger failCount = new AtomicInteger();
+        ParkOperInfo parkOperInfo = parkOperInfoRepository.findByParkInfoId(1L).get();
+        createUser(numOfUsers);
+        ParkSpaceInfo parkSpaceInfo = mgtService.getParkSpaceInfo(parkOperInfo);
+        ParkSpaceInfo useSpaceInfo = mgtService.getUseSpaceInfo(parkInfo);
+        BookingInfoRequestDto bookingInfoRequestDto = new BookingInfoRequestDto();
+        bookingInfoRequestDto.setStartDate(LocalDateTime.now());
+        bookingInfoRequestDto.setEndDate(LocalDateTime.now().plusHours(1));
+
+        List<User> userList = new ArrayList<>();
+        for (long i = 1; i <= numOfUsers; i++) {
+            userList.add(userRepository.findById(i).get());
+        }
+
+        //given
+        for (User user: userList){
+            executorService.execute(() -> {
+                try {
+                    bookingService.bookingPark(parkInfo.getId(),bookingInfoRequestDto,user);
+                    successCount.getAndIncrement();
+                    log.info("예약 성공 | 예약유저 : {}", user.getUserId());
+                } catch (CustomException e) {
+                    failCount.getAndIncrement();
+                    log.info(e.getMessage());
+                }
+                //스레드가 끝나면 -1 을 해서 endLatch를 1 줄임.
+                endLatch.countDown();
+            });
+        }
+
+        // Then
+        log.info("enter 동시성 테스트 결과 검증");
+        // 모든 스레드들이 끝날때까지 대기. 모든 스레드들이 끝나면 다음 코드 실행, 즉 endLatch가 0이 되면 다음 코드 실행
+        endLatch.await();
+        log.info("예약 성공 개수: {}", successCount.get());
+        Assertions.assertEquals(parkSpaceInfo.getBookingCarSpace()-useSpaceInfo.getBookingCarSpace(), successCount.get());
+        log.info("예약 실패 개수: {}", failCount.get());
+        Assertions.assertEquals(numOfUsers-parkSpaceInfo.getBookingCarSpace()-useSpaceInfo.getBookingCarSpace(), failCount.get());
+    }
+
+    private void createUser(int count){
+        List<User> userList = new ArrayList<>();
+        List<Car> carList = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            User user = User.of("user"+i,"1234");
+            userList.add(user);
+            carList.add(Car.of("11가1"+String.format("%03d", i + 1),user,true ));
+        }
+        userRepository.saveAll(userList);
+        carRepository.saveAll(carList);
+    }
+
+}
---
+++ src/test/java/com/sparta/parknav/management/service/MgtServiceTest.java
@@ -0,0 +1,112 @@
+package com.sparta.parknav.service;
+import com.sparta.parknav._global.exception.CustomException;
+import com.sparta.parknav.management.dto.ParkSpaceInfo;
+import com.sparta.parknav.management.dto.request.CarNumRequestDto;
+
+import com.sparta.parknav.management.service.MgtService;
+import com.sparta.parknav.parking.entity.ParkInfo;
+import com.sparta.parknav.parking.entity.ParkOperInfo;
+import com.sparta.parknav.parking.repository.ParkInfoRepository;
+import com.sparta.parknav.parking.repository.ParkOperInfoRepository;
+import com.sparta.parknav.user.entity.Admin;
+
+import com.sparta.parknav.user.repository.AdminRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class MgtServiceTest {
+
+    private static final Logger log = LoggerFactory.getLogger(com.sparta.parknav.service.MgtServiceTest3.class);
+    @Autowired
+    private MgtService mgtService;
+    @Autowired
+    private ParkInfoRepository parkInfoRepository;
+    @Autowired
+    private ParkOperInfoRepository parkOperInfoRepository;
+    @Autowired
+    private AdminRepository adminRepository;
+
+    @Test
+    public void testEnter() throws NoSuchFieldException, IllegalAccessException, InterruptedException {
+
+        // Given
+        log.info("enter 동시성 테스트 시작");
+        // admin 객체를 생성하기 위한 parkInfo 생성
+        ParkInfo parkInfo = parkInfoRepository.save(ParkInfo.of("테스트주차장", "테스트주소1", "테스트주소2", "33.2501489768202", "126.563230508718"));
+        ParkOperInfo parkOperInfoTmp = ParkOperInfo.of(parkInfo, "민영");
+        parkOperInfoTmp.update("00:00", "23:59","00:00", "23:59","00:00", "23:59",30, 1000, 30, 500, 10);
+        ParkOperInfo parkOperInfo = parkOperInfoRepository.save(parkOperInfoTmp);
+        Admin admin = Admin.of("admin", "1234", parkInfo);
+        adminRepository.save(admin);
+        ParkSpaceInfo parkSpaceInfo = mgtService.getParkSpaceInfo(parkOperInfo);
+        ParkSpaceInfo useSpaceInfo = mgtService.getUseSpaceInfo(parkInfo);
+
+        int numOfUsers = 20;
+        // 대기하는 스레드의 숫자를 지정
+        CountDownLatch endLatch = new CountDownLatch(numOfUsers);
+        List<CarNumRequestDto> vehicleIds = generateVehicleIds(numOfUsers);
+        // 20개의 스레드 생성
+        ExecutorService executorService = Executors.newFixedThreadPool(numOfUsers);
+        // 원자적인 연산을 보장해주는 Atomic 패키지 클래스로 멀티 스레드 환경에서 여러 스레드들이 동시에 값을 변경할 수 없도록 안전성 보장
+        AtomicInteger successCount = new AtomicInteger();
+        AtomicInteger failCount = new AtomicInteger();
+
+        // When
+        log.info("enter 동시성 테스트 진행");
+
+        for (CarNumRequestDto vehicleId : vehicleIds) {
+            executorService.execute(() -> {
+                try {
+                    mgtService.enter(vehicleId, admin);
+                    successCount.getAndIncrement();
+                    log.info("입차 성공 | 차량 아이디 : {}", vehicleId.getCarNum());
+                } catch (CustomException e) {
+                    System.out.println(e.getMessage());
+                    failCount.getAndIncrement();
+                }
+                //스레드가 끝나면 -1 을 해서 endLatch를 1 줄임.
+                endLatch.countDown();
+            });
+        }
+
+        // Then
+        log.info("enter 동시성 테스트 결과 검증");
+        // 모든 스레드들이 끝날때까지 대기. 모든 스레드들이 끝나면 다음 코드 실행, 즉 endLatch가 0이 되면 다음 코드 실행
+        endLatch.await();
+        Assertions.assertEquals(parkSpaceInfo.getGeneralCarSpace()+parkSpaceInfo.getCommonCarSpace(), successCount.get());
+        log.info("입차 성공 개수: {}", successCount.get());
+        Assertions.assertEquals(numOfUsers - parkSpaceInfo.getGeneralCarSpace()-parkSpaceInfo.getCommonCarSpace(), failCount.get());
+        log.info("입차 실패 개수: {}", failCount.get());
+    }
+
+    private List<CarNumRequestDto> generateVehicleIds(int count) throws NoSuchFieldException, IllegalAccessException {
+        List<CarNumRequestDto> vehicleIds = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            CarNumRequestDto carNumRequestDto = new CarNumRequestDto();
+            Field parkIdField = CarNumRequestDto.class.getDeclaredField("parkId");
+            parkIdField.setAccessible(true);
+            parkIdField.set(carNumRequestDto, 1L);
+
+            Field carNumField = CarNumRequestDto.class.getDeclaredField("carNum");
+            carNumField.setAccessible(true);
+            carNumField.set(carNumRequestDto, "11가1" + String.format("%03d", i + 1));
+            vehicleIds.add(carNumRequestDto);
+        }
+        return vehicleIds;
+    }
+}
