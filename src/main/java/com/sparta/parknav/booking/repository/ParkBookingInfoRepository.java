7139715d049d02e951aff1a704b00efe2bab6f5c
Seyoung Oh <osy9536@naver.com>
2023-04-17 00:24:45 +0900

    Fix : endTime을 exitTime으로 변경

--- src/main/java/com/sparta/parknav/booking/repository/ParkBookingInfoRepository.java
+++ src/main/java/com/sparta/parknav/booking/repository/ParkBookingInfoRepository.java
@@ -17,10 +17,10 @@ public interface ParkBookingInfoRepository extends JpaRepository<ParkBookingInfo
 
     List<ParkBookingInfo> findAllByParkInfoIdAndUserId(Long parkInfoId, Long userId);
 
-    Optional<ParkBookingInfo> findTopByParkInfoIdAndCarNumAndStartTimeLessThanEqualAndEndTimeGreaterThan(Long parkInfoId, String carNum, LocalDateTime startTime, LocalDateTime endTime);
+    Optional<ParkBookingInfo> findTopByParkInfoIdAndCarNumAndStartTimeLessThanEqualAndExitTimeGreaterThan(Long parkInfoId, String carNum, LocalDateTime startTime, LocalDateTime exitTime);
 
     @Query(value = "SELECT p from ParkBookingInfo p WHERE p.parkInfo.id = :parkId AND p.carNum = :carNum " +
-            "AND ((p.startTime >= :startTime and p.startTime < :endTime) OR (p.endTime > :startTime and p.endTime <= :endTime))")
+            "AND ((p.startTime >= :startTime and p.startTime < :endTime) OR (p.exitTime > :startTime and p.exitTime <= :endTime))")
     ParkBookingInfo getAlreadyBookingInfo(@Param("parkId") Long parkId, @Param("carNum") String carNum, @Param("startTime") LocalDateTime startTime, @Param("endTime") LocalDateTime endTime);
 
     ParkBookingInfo findTopByParkInfoIdAndCarNumAndStartTimeGreaterThan(Long id, String carNum, LocalDateTime now);
